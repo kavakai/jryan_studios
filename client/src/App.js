1ac5c768e477729a9f4d48c72093513f0c069bc5
Kai Kuller <kavakai@gmail.com>
2023-09-02 14:30:30 -0600

    Update App.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Home from "./Components/home/Home";
+import Navbar from "./Components/global/Navbar";
+import ItemDetails from "./Components/itemDetails/ItemDetails";
+import CartMenu from "./Components/global/CartMenu";
+import Checkout from "./Components/checkout/Checkout";
+import Confirmation from "./Components/checkout/Confirmation";
+import Footer from "./Components/global/Footer";
+import About from "./Components/about/About";
+import Stockists from "./Components/stockists/Stockists";
+import Cancel from "./Components/checkout/Cancel";
+import React from "react";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <BrowserRouter>
+        <Navbar />
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="item/:itemId" element={<ItemDetails />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="checkout/success" element={<Confirmation />} />
+          <Route path="checkout/cancel" element={<Cancel />} />
+          <Route path="about" element={<About />} />
+          <Route path="stockists" element={<Stockists />} />
+        </Routes>
+        <CartMenu />
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
