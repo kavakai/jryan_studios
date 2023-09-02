24582670a45e29cb109c559483ddca03ab6574c2
Kai Kuller <kavakai@gmail.com>
2023-09-02 15:45:31 -0600

    Delete and reinstall packagelock

---
+++ src/api/stockist/controllers/stockist.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * stockist controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::stockist.stockist');
