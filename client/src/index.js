1092841ca615fb147cc8c47946896ae4c193f6e1
Kai Kuller <kavakai@gmail.com>
2023-09-04 13:48:27 -0600

    Try and fix import in index

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import { CssBaseline } from '@mui/material';
 import { theme } from './theme';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './components/state/cartSlice';
+import { cartReducer } from './components/state';
 
 const store = configureStore({
   reducer: { cart: cartReducer },
