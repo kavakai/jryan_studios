c6f9f8cca43ec1fc07cbb9cd49d276cc2e67d2e2
Kai Kuller <kavakai@gmail.com>
2023-09-04 11:31:57 -0600

    Change to live key

--- client/src/Components/checkout/Checkout.jsx
+++ client/src/Components/checkout/Checkout.jsx
@@ -31,8 +31,8 @@ const Checkout = () => {
   };
   
   const stripePromise = loadStripe(
-    'pk_test_51NDsOeGc9Mev9oaLpLcF8txfRm5sFdOuoyZWyT3ZQKa4MhC2TjW055Q1CUU2piUc9X3SO7YNXn7BtcW7lA0b47W500xfwiehRN'
-    // 'pk_live_51NDsOeGc9Mev9oaLiRxVP47oV3qHuGnP9mTSE2NNIyTBmG7xPZSztxxdcj6bkOE8ZxmEbqJJUVCHCIv1ITcBydK200cY1wrJ99'
+    // 'pk_test_51NDsOeGc9Mev9oaLpLcF8txfRm5sFdOuoyZWyT3ZQKa4MhC2TjW055Q1CUU2piUc9X3SO7YNXn7BtcW7lA0b47W500xfwiehRN'
+    'pk_live_51NDsOeGc9Mev9oaLiRxVP47oV3qHuGnP9mTSE2NNIyTBmG7xPZSztxxdcj6bkOE8ZxmEbqJJUVCHCIv1ITcBydK200cY1wrJ99'
     );
   if(process.env.STRAPI_ADMIN_STRIPE_KEY) { 
     console.log('It is set!'); 
