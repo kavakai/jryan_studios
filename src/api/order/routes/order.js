2bde27d800548812f01a9002c0fb7f64778fc4b3
Kai Kuller <kavakai@gmail.com>
2023-09-08 12:11:30 -0600

    Add env vars to strapi deployed app

--- src/api/order/routes/order.js
+++ src/api/order/routes/order.js
@@ -6,4 +6,10 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::order.order');
+module.exports = createCoreRouter('api::order.order', {
+  config: {
+    find: {
+      auth: false
+    }
+  }
+});
